0cf78d9d7c7104b1d8e74eb4fa0e7bf7bdd44211
Abdullah M <abdullah@goejaza.com>
2016-07-27 20:39:28 +0800

    use alert() helper make errors support string and array

--- src/SweetAlert/ConvertMessagesIntoSweatAlert.php
+++ src/SweetAlert/ConvertMessagesIntoSweatAlert.php
@@ -17,29 +17,32 @@ class ConvertMessagesIntoSweatAlert
     public function handle($request, Closure $next)
     {
         if($request->session()->has('success')){
-            Alert::success($request->session()->get('success'), 'Success')->persistent();
+            alert()->success($request->session()->get('success'), 'Success')->persistent();
         }
 
         if($request->session()->has('warning')){
-            Alert::success($request->session()->get('warning'), 'Warning')->persistent();
+            alert()->success($request->session()->get('warning'), 'Warning')->persistent();
         }
 
         if($request->session()->has('info')){
-            Alert::success($request->session()->get('info'), 'Info')->persistent();
+            alert()->success($request->session()->get('info'), 'Info')->persistent();
         }
 
         if($request->session()->has('message')){
-            Alert::success($request->session()->get('message'))->persistent();
+            alert()->success($request->session()->get('message'))->persistent();
         }
 
         if($request->session()->has('basic')){
-            Alert::success($request->session()->get('basic'));
+            alert()->success($request->session()->get('basic'));
         }
 
         if($request->session()->has('errors')){
-            $message = $this->prepareErrors($request->session()->get('errors')->getMessages());
+            $message = $request->session()->get('errors');
+            if(!is_string($message)){
+                $message = $this->prepareErrors($message->getMessages());
+            }
 
-            Alert::error($message, 'Errors')->html()->persistent();
+            alert()->error($message, 'Errors')->html()->persistent();
         }
 
         return $next($request);
