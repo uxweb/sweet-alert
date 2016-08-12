bdc7d7d89fc2378643c2b8ba0bf31f4ef6a3e587
Uziel Bueno <ux.webs@gmail.com>
2016-08-11 19:44:51 -0500

    Added some fixes ;).

--- src/SweetAlert/ConvertMessagesIntoSweatAlert.php
+++ src/SweetAlert/ConvertMessagesIntoSweatAlert.php
@@ -3,7 +3,6 @@
 namespace UxWeb\SweetAlert;
 
 use Closure;
-use SweetAlertNotifier as Alert;
 
 class ConvertMessagesIntoSweatAlert
 {
@@ -16,33 +15,34 @@ class ConvertMessagesIntoSweatAlert
      */
     public function handle($request, Closure $next)
     {
-        if($request->session()->has('success')){
-            alert()->success($request->session()->get('success'), 'Success')->persistent();
+        if ($request->session()->has('success')) {
+            alert()->success($request->session()->get('success'))->persistent();
         }
 
-        if($request->session()->has('warning')){
-            alert()->success($request->session()->get('warning'), 'Warning')->persistent();
+        if ($request->session()->has('warning')) {
+            alert()->warning($request->session()->get('warning'))->persistent();
         }
 
-        if($request->session()->has('info')){
-            alert()->success($request->session()->get('info'), 'Info')->persistent();
+        if ($request->session()->has('info')) {
+            alert()->info($request->session()->get('info'))->persistent();
         }
 
-        if($request->session()->has('message')){
-            alert()->success($request->session()->get('message'))->persistent();
+        if ($request->session()->has('message')) {
+            alert()->message($request->session()->get('message'))->persistent();
         }
 
-        if($request->session()->has('basic')){
-            alert()->success($request->session()->get('basic'));
+        if ($request->session()->has('basic')) {
+            alert()->basic($request->session()->get('basic'));
         }
 
-        if($request->session()->has('errors')){
+        if ($request->session()->has('errors')) {
             $message = $request->session()->get('errors');
-            if(!is_string($message)){
+
+            if (!is_string($message)) {
                 $message = $this->prepareErrors($message->getMessages());
             }
 
-            alert()->error($message, 'Errors')->html()->persistent();
+            alert()->error($message)->html()->persistent();
         }
 
         return $next($request);
@@ -54,10 +54,10 @@ class ConvertMessagesIntoSweatAlert
      * @param $errors
      * @return string
      */
-    private function prepareErrors($errors){
+    private function prepareErrors($errors)
+    {
         $errors = collect($errors);
 
         return $errors->flatten()->implode('<br />');
     }
 }
-
