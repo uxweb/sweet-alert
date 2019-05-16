8fe778f7430e8c16c929f62a200e5d6c7040873d
MeijdenMedia <joost@meijdenmedia.nl>
2019-05-16 09:05:31 +0200

    Made the config method compatible with Laravels method

--- tests/SweetAlertNotifierTest.php
+++ tests/SweetAlertNotifierTest.php
@@ -366,7 +366,24 @@ class SweetAlertNotifierTest extends TestCase
     }
 }
 
-function config($key, $default)
+/**
+ * Get / set the specified configuration value.
+ *
+ * If an array is passed as the key, we will assume you want to set an array of values.
+ *
+ * @param  array|string  $key
+ * @param  mixed  $default
+ * @return mixed|\Illuminate\Config\Repository
+ */
+function config($key = null, $default = null)
 {
-    return 1800;
+    if (is_null($key)) {
+        return app('config');
+    }
+
+    if (is_array($key)) {
+        return app('config')->set($key);
+    }
+
+    return app('config')->get($key, $default);
 }
