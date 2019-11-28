ee3bf9e45be9392480695618e7a23c9d2b15f5dd
Nelson Martell <nelson6e65@gmail.com>
2019-11-28 14:34:59 -0500

    :memo: Add DockBlocks to facade methods for IDE autocompletion

--- src/SweetAlert/SweetAlert.php
+++ src/SweetAlert/SweetAlert.php
@@ -4,6 +4,14 @@ namespace UxWeb\SweetAlert;
 
 use Illuminate\Support\Facades\Facade;
 
+/**
+ * @method static \UxWeb\SweetAlert\SweetAlertNotifier message(string $text = '', $title = null, $icon = null)
+ * @method static \UxWeb\SweetAlert\SweetAlertNotifier basic(string $text, string $title)
+ * @method static \UxWeb\SweetAlert\SweetAlertNotifier info(string $text, string $title = '')
+ * @method static \UxWeb\SweetAlert\SweetAlertNotifier success(string $text, string $title = '')
+ * @method static \UxWeb\SweetAlert\SweetAlertNotifier error(string $text, string $title = '')
+ * @method static \UxWeb\SweetAlert\SweetAlertNotifier warning(string $text, string $title = '')
+ */
 class SweetAlert extends Facade
 {
     /**
